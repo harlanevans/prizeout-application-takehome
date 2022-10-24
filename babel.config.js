6d67fae82fc8754f0f6a3be82643b402bbedadfb
harlan evans <harlanevans5@gmail.com>
2022-10-24 11:25:24 -0700

    adjust babel versions and plugins to get test to work.

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,20 @@
 module.exports = {
     presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
-        },
-      ],
-      '@babel/preset-react',
-      '@babel/preset-typescript'
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current',
+                },
+            },
+        ],
+        '@babel/preset-react',
+        '@babel/preset-typescript',
     ],
     plugins: [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-transform-runtime',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-transform-runtime',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-transform-destructuring',
     ],
-  };
+};
